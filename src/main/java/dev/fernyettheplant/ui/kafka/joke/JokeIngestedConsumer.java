068fb475067aa112d029a1527d76919999b08617
Jesus Fernando <masterofer36@gmail.com>
2024-01-28 10:49:07 -0500

    chore: configure kafka listener

--- src/main/java/dev/fernyettheplant/ui/kafka/joke/JokeIngestedConsumer.java
+++ src/main/java/dev/fernyettheplant/ui/kafka/joke/JokeIngestedConsumer.java
@@ -7,7 +7,7 @@ import org.jboss.logging.Logger;
 @ApplicationScoped
 public class JokeIngestedConsumer {
     private static final Logger log = Logger.getLogger(JokeIngestedConsumer.class);
-    
+
     @Incoming("joke-ingested-in")
     public void consume(byte[] message) {
         log.info("Message received");
